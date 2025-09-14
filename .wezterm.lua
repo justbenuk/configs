aea0ecb17aab5487423456322c7548d0766ca6f4
justbenuk <justbenuk@gmail.com>
2025-09-14 19:34:38 +0100

    initial commit

---
+++ .wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+local config = {}
+config.font_size = 10
+config.color_scheme = "nord"
+
+-- top/window bars
+config.window_decorations = "NONE"
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
